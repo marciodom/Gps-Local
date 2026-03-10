03c094cb02f94be3924ea87f7470c7d5056666d8
Marcio Dom <marciodom98l@gmail.com>
2026-03-10 12:31:38 -0300

    Primeiro commit - GPS Local

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'gps-alarme-v3';
+const ASSETS = [
+  '/Gps-Local/',
+  '/Gps-Local/index.html',
+  '/Gps-Local/manifest.json',
+  '/Gps-Local/audio/alarme.mp3',
+  '/Gps-Local/icons/icon-192.png',
+  '/Gps-Local/icons/icon-512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css',
+  'https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
